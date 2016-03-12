c1b758bd383be48a662c255e61cba645f355e4df
HivenKay <hivenkay@gmail.com>
2016-03-12 13:49:58 +0800

    Fisrt Commit

---
+++ aes.go
@@ -0,0 +1,47 @@
+package libs
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func Encrypt(plantText, key []byte) ([]byte, error) {
+	// AES-128。key长度：16, 24, 32 bytes 对应 AES-128, AES-192, AES-256
+	block, err := aes.NewCipher(key) //选择加密算法
+	if err != nil {
+		return nil, err
+	}
+	plantText = PKCS7Padding(plantText, block.BlockSize())
+
+	blockModel := cipher.NewCBCEncrypter(block, key)
+
+	ciphertext := make([]byte, len(plantText))
+
+	blockModel.CryptBlocks(ciphertext, plantText)
+	return ciphertext, nil
+}
+
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+func Decrypt(ciphertext, key []byte) ([]byte, error) {
+	keyBytes := []byte(key)
+	block, err := aes.NewCipher(keyBytes) //选择加密算法
+	if err != nil {
+		return nil, err
+	}
+	blockModel := cipher.NewCBCDecrypter(block, keyBytes)
+	plantText := make([]byte, len(ciphertext))
+	blockModel.CryptBlocks(plantText, ciphertext)
+	plantText = PKCS7UnPadding(plantText, block.BlockSize())
+	return plantText, nil
+}
+
+func PKCS7UnPadding(plantText []byte, blockSize int) []byte {
+	length := len(plantText)
+	unpadding := int(plantText[length-1])
+	return plantText[:(length - unpadding)]
+}
---
+++ string.go
@@ -0,0 +1,60 @@
+package libs
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"fmt"
+	r "math/rand"
+	"regexp"
+	"time"
+)
+
+var emailPattern = regexp.MustCompile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[a-zA-Z0-9](?:[\\w-]*[\\w])?")
+
+func Md5(buf []byte) string {
+	hash := md5.New()
+	hash.Write(buf)
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func SizeFormat(size float64) string {
+	units := []string{"Byte", "KB", "MB", "GB", "TB"}
+	n := 0
+	for size > 1024 {
+		size /= 1024
+		n += 1
+	}
+
+	return fmt.Sprintf("%.2f %s", size, units[n])
+}
+
+func IsEmail(b []byte) bool {
+	return emailPattern.Match(b)
+}
+
+// RandomCreateBytes generate random []byte by specify chars.
+func RandomCreateBytes(n int, alphabets ...byte) []byte {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	var randby bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randby = true
+	}
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			if randby {
+				bytes[i] = alphanum[r.Intn(len(alphanum))]
+			} else {
+				bytes[i] = alphanum[b%byte(len(alphanum))]
+			}
+		} else {
+			if randby {
+				bytes[i] = alphabets[r.Intn(len(alphabets))]
+			} else {
+				bytes[i] = alphabets[b%byte(len(alphabets))]
+			}
+		}
+	}
+	return bytes
+}
